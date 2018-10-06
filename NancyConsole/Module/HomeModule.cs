7b23aef0190b40eb34835de8ffe3c93eb8cfddc2
robinjiang <me@robinjiang.com>
2018-10-06 12:38:46 +0800

    init nancyfx demo
    init demo

---
+++ NancyConsole/Module/HomeModule.cs
@@ -0,0 +1,21 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NancyConsole.Module
+{
+    public class HomeModule:NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = r =>
+            {
+                var os = System.Environment.OSVersion;
+                return "Hello Nancy<br/> System:" + os.VersionString;
+            };
+        }
+    }
+}
---
+++ NancyFxDemo/Modules/HomeModule.cs
@@ -0,0 +1,19 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NancyFxDemo.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = p =>
+            {
+                return View["/Home"];
+            };
+        }
+    }
+}
